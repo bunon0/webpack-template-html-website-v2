c99178ba57901d0a8e2bcbdf9bcfdeb7b1a56c7b
bunon0 <86172592+Nobu-ur@users.noreply.github.com>
2022-02-01 08:56:03 +0900

    ✨ 画像圧縮の設定　#3

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const ImageminMozjpeg = require("imagemin-mozjpeg");
 
 module.exports = {
-  entry: `./src/js/index.js`,
+  mode: "development",
+  entry: "./src/js/index.js",
   output: {
-    path: `${__dirname}/dist`,
+    path: path.resolve(__dirname, "dist"),
     filename: "js/main.js",
   },
-  mode: "development",
 
   devServer: {
     static: "dist",
@@ -18,20 +22,11 @@ module.exports = {
     // watchFiles: ["src/templates/**/*"],
   },
 
-  //パッケージのライセンス情報をjsファイルの中に含める
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-      new CssMinimizerPlugin(),
-    ],
-  },
-
   module: {
     rules: [
       {
         test: /\.js$/,
+        include: path.resolve(__dirname, "src/js/"),
         use: [
           {
             loader: "babel-loader",
@@ -52,17 +47,28 @@ module.exports = {
       // },
       {
         test: /\.scss$/,
-        include: path.resolve(__dirname, "src/scss/"),
+        include: path.resolve(__dirname, "./src/scss/"),
         use: [
-          MiniCssExtractPlugin.loader,
+          { loader: MiniCssExtractPlugin.loader },
           {
             loader: "css-loader",
             options: {
               url: false,
+              sourceMap: true,
             },
           },
-          { loader: "postcss-loader" },
-          { loader: "sass-loader" },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
           { loader: "glob-import-loader" },
         ],
       },
@@ -73,10 +79,53 @@ module.exports = {
 
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ["**/*", "!**.html"],
+      cleanOnceBeforeBuildPatterns: ["**/*"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: "css/styles.css",
+      filename: "./css/styles.css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: `${path.resolve(__dirname, "src")}/images/`,
+          to: `${path.resolve(__dirname, "dist")}/images/`,
+        },
+      ],
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      plugins: [
+        ImageminMozjpeg({
+          quality: 89,
+          progressive: true,
+        }),
+      ],
+      pngquant: {
+        quality: "80-89",
+      },
+      gifsicle: {
+        interlaced: false,
+        optimizationLevel: 10,
+        colors: 256,
+      },
+      svgo: {},
     }),
   ],
+
+  //パッケージのライセンス情報をjsファイルの中に含める
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin(),
+    ],
+  },
+
+  watchOptions: {
+    ignored: /node_modules/,
+  },
 };
